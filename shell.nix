d2ef46d2c05a6ead3040356ab0b8c9dabd25743d
vytaro <vitorgabriels1000@gmail.com>
2026-04-19 21:34:20 -0300

    feat(structure): add initial project folders and files

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{
+  pkgs ? import <nixpkgs> {
+    config.allowUnfree = true;
+  },
+}:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    nodejs
+    php
+    php.packages.composer
+    laravel
+    nodePackages.intelephense
+    vtsls
+    vscode-langservers-extracted
+  ];
+}
