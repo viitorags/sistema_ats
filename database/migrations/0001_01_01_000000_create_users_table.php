7ecc83cf43c4998e52dfcc231d3c82b1e52e2952
vytaro <vitorgabriels1000@gmail.com>
2026-04-22 19:58:24 -0300

    feat(database/migrations): add gemini_key column to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('gemini_key');
             $table->rememberToken();
             $table->timestamps();
         });
