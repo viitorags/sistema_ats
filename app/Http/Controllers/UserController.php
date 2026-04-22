27922d7ddef492785ee1e52c6ac8002638a23b9d
vytaro <vitorgabriels1000@gmail.com>
2026-04-22 20:11:57 -0300

    feat(controller, requests, resources): implementa UserController com métodos de login, cadastro, atualização e exclusão;
    adiciona classes de validação de solicitação correspondentes e representação de recursos

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::where('email', $data['email'])->first();
+
+        if (! $user || ! Hash::check($data['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Credenciais Inválidas'
+            ], 401);
+        }
+
+        return new UserResource($user);
+    }
+
+    public function register(StoreUserRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create($data);
+        return new UserResource($user);
+    }
+
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $data = $request->validated();
+
+        if (isset($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+
+        $user->update($data);
+        return new UserResource($user);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json(['message' => 'Usuário excluido com sucesso'], 200);
+    }
+}
---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['sometimes', 'required', 'string', 'max:255'],
+            'email' => ['sometimes', 'required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($this->route('user'))],
+            'password' => ['nullable', 'string', 'min:8'],
+        ];
+    }
+}
